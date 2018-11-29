022478cfa6b623e69f2a5096d3d20b48033280e9
Bobrov Vlad <vladbobrov3@gmail.com>
2018-11-29 20:29:40 +0200

    Save the Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import './scss/main.scss';
+
 console.log('Hello!');
 
 console.log(`The time is ${new Date()}`);
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -26,3 +26,30 @@ module.exports = {
         ],
     },
 };
+
+// ... contents of webpack.config.js
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    // ...previous Webpack config...
+    module: {
+        rules: [
+            // ...additional rules...
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    // ...previous Webpack config...
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
+};
+
